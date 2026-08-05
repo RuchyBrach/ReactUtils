096942cb36a4eeaac23f67d06e0fc00d624e55b2
ruchybrach <brach4a@gmail.com>
2026-08-05 14:02:25 -0400

    added use sessiontimout hook and bug fix for logout

--- src/UserStore.ts
+++ src/UserStore.ts
@@ -43,7 +43,7 @@ export function getUserStore(apiurl: string) {
                     ...initialvals,
                     logout: async (username: string) => {
                         const user = await logoutUser(apiurl, username);
-                        const newstate = { userName: user.userName, roleName: user.roleName, roleRank: user.roleRank, sessionKey: user.sessionKey, errorMessage: user.errorMessage, isLoggedIn: false };
+                        const newstate = { userName: "", roleName: "", roleRank: 0, sessionKey: "", errorMessage: user.errorMessage, isLoggedIn: false };
                         sessionStorage.setItem(keyname, JSON.stringify(newstate));
                         set(newstate);
                     },
---
+++ src/useSessionTimeout.ts
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { getUserStore } from "./UserStore";
+import { Navigate } from "react-router-dom";
+import { useIdleTimer } from "react-idle-timer";
+
+interface SessionTimeoutProps {
+    apiurl: string;
+    timeout: number;
+    pathtologin: string;
+}
+
+export function useSessionTimeout({ apiurl, timeout, pathtologin }: SessionTimeoutProps) {
+    const useUserStore = getUserStore(apiurl);
+    const isLoggedIn = useUserStore(state => state.isLoggedIn);
+    const logout = useUserStore((state) => state.logout);
+    const username = useUserStore((state) => state.userName);
+
+    const handleOnIdle = async () => {
+        if (isLoggedIn) {
+            await logout(username);
+            if (pathtologin) {
+                Navigate({ to: pathtologin });
+            }
+        }
+    };
+
+    const { start, reset } = useIdleTimer({
+        timeout,
+        onIdle: handleOnIdle,
+        debounce: 500
+    });
+
+    useEffect(() => {
+        if (isLoggedIn) {
+            start();
+        }
+        else {
+            reset();
+        }
+    }, [isLoggedIn, start, reset]);
+}
